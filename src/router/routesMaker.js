851ee2528efd18b37006b03994c8c9ea26f1057a
stonehong <740079600@qq.com>
2018-06-12 09:17:13 +0800

    6.12

--- src/router/routesMaker.js
+++ src/router/routesMaker.js
@@ -2,6 +2,7 @@
 //只需在map文件夹定义不同的路由,即可自动import进此项目
 import route0 from "./map/LoginPage/index.js"
 import route1 from "./map/System/PlaceManager/index.js"
+import route2 from "./map/System/resource/index.js"
 let routes = []
-let myRoutes = routes.concat(route0,route1)
+let myRoutes = routes.concat(route0,route1,route2)
 export default myRoutes;
